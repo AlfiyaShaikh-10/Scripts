8c452f8e0198dfdc319bc53bd5c03f3d0dba68bc
Alfiya Shaikh <61371311+AlfiyaShaikh-10@users.noreply.github.com>
2021-06-19 17:41:25 +0530

    Update Ping_IPs(iterative).sh

--- BashScriprts/Ping_IPs(iterative).sh
+++ BashScriprts/Ping_IPs(iterative).sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-for third in {0..6}
+for third in {0..255}
 do
-	for fourth in {0..10}
+	for fourth in {0..255}
     do	
 	    ping -c 2 127.0.$third.$fourth >> results.txt  && echo 127.0.$third.$fourth is up 
       #replace with the IP Address you wanna scan..
