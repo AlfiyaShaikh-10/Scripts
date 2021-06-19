e7b79eaeb1ac309787e852ca3d777589e7623bd0
Alfiya Shaikh <61371311+AlfiyaShaikh-10@users.noreply.github.com>
2021-06-19 16:16:22 +0530

    Update NmapScanOn_IPs.sh

--- BashScriprts/NmapScanOn_IPs.sh
+++ BashScriprts/NmapScanOn_IPs.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 Nmap()
 {
+VAR='\' ;
 while read -r line
 do
 name="$line"
@@ -8,8 +9,14 @@ echo "$name"
 count=$line
 
 sudo nmap -sC -sV -p- -T4 -f -iL iplist.txt $line1 
+
+if [ $VAR!='\' ];
+then echo "all scans are performed"
+break
+fi
+
 done < iplist.txt
 
-# provide the list of IPs in "iplist.txt" 
+# provide the list of IPs in "iplist.txt" and after listing all the IPs type "\" at the EOF.
 }
 Nmap
